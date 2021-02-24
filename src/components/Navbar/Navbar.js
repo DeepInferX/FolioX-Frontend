6bfb2fe3f694963d1d0774674d29cceaf5ac9d9e
ADILZAMAL <adilzamal@gmail.com>
2021-02-24 22:33:14 +0530

    responsive navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import classNames from "classnames";
-import useStyle from "assets/style/components/Navbar/Navbar";
+import { makeStyles } from "@material-ui/core";
+
+//style
+const useStyle = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: `${theme.spacing(5)}px`,
+    [theme.breakpoints.down("sm")]: {
+      padding: [theme.spacing(2)],
+      paddingTop: [theme.spacing(5)],
+    },
+  },
+  navLink: {
+    textDecoration: "none",
+  },
+  navLinkButton: {
+    marginLeft: theme.spacing(5),
+    backgroundColor: "#F0E9E1",
+    borderRadius: theme.spacing(5),
+    padding: `0px ${theme.spacing(4)}px`,
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    textTransform: "uppercase",
+  },
+}));
+
 export default function Navbar({ logo, appbar }) {
   const classes = useStyle();
   return (
