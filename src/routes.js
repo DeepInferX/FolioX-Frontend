ae958d836f5a6474206430564627a3f6a5107e8a
ADILZAMAL <adilzamal@gmail.com>
2021-03-03 19:07:44 +0530

    merged

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Landing from "pages/Landing/Landing";
 import GetStarted from "pages/GetStarted/GetStarted";
 import AdminLogin from "pages/Admin/AdminLogin/AdminLogin";
@@ -37,40 +36,3 @@ const routes = [
 ];
 
 export default routes;
-=======
-import Landing from "pages/Landing/Landing";
-import GetStarted from "pages/GetStarted/GetStarted";
-import AdminLogin from "pages/Admin/AdminLogin/AdminLogin";
-import AdminRegister from "pages/Admin/AdminRegister/AdminRegister";
-import AdminOption from "pages/Admin/AdminOption/AdminOption";
-import AdminRegistrationOverlay from "pages/Admin/AdminRegister/RegistrationOverlay";
-import AdminContext from "pages/Admin/AdminContext";
-import Dashboard from "pages/Admin/Dashboard/DashboardLayout";
-import DashboardHome from "pages/Admin/Dashboard/view/Home";
-
-const routes = [
-  {
-    path: "/",
-    children: [
-      { path: "/", element: <Landing /> },
-      { path: "/get-started", element: <GetStarted /> },
-    ],
-  },
-  {
-    path: "admin",
-    element: <AdminContext />,
-    children: [
-      { path: "/", element: <AdminLogin /> },
-      { path: "login", element: <AdminLogin /> },
-      { path: "register", element: <AdminRegister /> },
-      { path: "option", element: <AdminOption /> },
-      { path: "registration-confirm-overlay", element: <AdminRegistrationOverlay />},
-      { path: "dashboard", element: ( <Dashboard /> ),
-       children: [{ path: "/", element: <DashboardHome /> }]
-      },
-    ],
-  },
-];
-
-export default routes;
->>>>>>> 77a5ae5fd88813ece8291b74064a68d97705abdf
